333be1af77d49e02db79d762a6df7abf423c46db
Maxim Safioulline <maxim@Maxims-MacBook-Pro.local>
2016-02-20 23:58:56 -0500

    Image updates

--- chestnuts.php
+++ chestnuts.php
@@ -13,12 +13,11 @@
       <div class="col-xs-12 col-md-8 col-md-offset-4">
         <img src="projects/chestnuts/chestnuts6.jpg" class="img-responsive center-block">
         <img src="projects/chestnuts/chestnuts7.jpg" class="img-responsive center-block">
+        <img src="projects/chestnuts/chestnuts5.jpg" class="img-responsive center-block">
         <img src="projects/chestnuts/chestnuts1.jpg" class="img-responsive center-block">
         <img src="projects/chestnuts/chestnuts2.jpg" class="img-responsive center-block">
         <img src="projects/chestnuts/chestnuts3.jpg" class="img-responsive center-block">
         <img src="projects/chestnuts/chestnuts4.jpg" class="img-responsive center-block">
-        <img src="projects/chestnuts/chestnuts5.jpg" class="img-responsive center-block">
-        <img src="projects/chestnuts/chestnuts8.jpg" class="img-responsive center-block">
       </div>
     </div>
   </section>
--- hahaha.php
+++ hahaha.php
@@ -14,11 +14,9 @@
         <img src="projects/hahaha/hahaha1.jpg" class="img-responsive center-block">
         <img src="projects/hahaha/hahaha2.jpg" class="img-responsive center-block">
         <img src="projects/hahaha/hahaha4.jpg" class="img-responsive center-block">
-        <img src="projects/hahaha/hahaha5.jpg" class="img-responsive center-block">
         <img src="projects/hahaha/hahaha6.jpg" class="img-responsive center-block">
         <img src="projects/hahaha/hahaha7.jpg" class="img-responsive center-block">
         <img src="projects/hahaha/hahaha8.jpg" class="img-responsive center-block">
-        <img src="projects/hahaha/hahaha9.jpg" class="img-responsive center-block">
       </div>
     </div>
   </section>
