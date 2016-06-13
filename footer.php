8886cf20a1efedbf123070d1b1d275b2277a4bea
Maxim Safioulline <maxim@Maxims-MacBook-Pro.local>
2016-06-12 20:41:04 -0400

    Updated footer and removed on the of images from logan page

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="row">
     <div class="col-xs-12">
-      <p><a href="hahaha.php">Hahaha</a>; <a href="upc.php">Until Perfect Comes</a>; <a href="selfmademan.php">Self Made Man</a>; <a href="chestnuts.php">Untitled (Loja)</a>; <a href="nautilus.php">Nautilus</a>; <a href="mommydidnt.php">Letting Go</a>; <a href="timepass.php">Timepass</a>; <a href="yap.php">YAP 2013</a>; <a href="cutnpaste.php">Cut'N'Paste</a>; <a href="humanrights.php">Canadian Human Rights Museum</a>; <a href="swirl.php">Untitled (swirl)</a>; <a href="scott.php">Between a Rock And a Romantic Place</a>; <a href="youarehere.php">You Are Here</a>; <a href="blue.php">Blue</a></p>
+      <p><a href="blue.php">Blue</a>; <a href="logan.php">Between Fields of Darkness</a>; <a href="hahaha.php">Hahaha</a>; <a href="upc.php">Until Perfect Comes</a>; <a href="selfmademan.php">Self Made Man</a>; <a href="listen.php">Listen magazine</a>; <a href="chestnuts.php">Untitled (Loja)</a>; <a href="nautilus.php">Nautilus</a>; <a href="mommydidnt.php">Letting Go</a>; <a href="timepass.php">Timepass</a>; <a href="yap.php">YAP 2013</a>; <a href="cutnpaste.php">Cut'N'Paste</a>; <a href="humanrights.php">Canadian Human Rights Museum</a>; <a href="adecker.php">Campaign</a>; <a href="swirl.php">Untitled (swirl)</a>; <a href="scott.php">Between a Rock And a Romantic Place</a>; <a href="youarehere.php">You Are Here</a></p>
     </div>
   </div>
 </footer>
--- logan.php
+++ logan.php
@@ -10,7 +10,6 @@
           spreads from artist book for logan acton</p>
       </div>
       <div class="col-xs-12 col-md-8 col-md-offset-4">
-        <img src="projects/logan/logan_cover.jpg" class="img-responsive center-block type-cover img-shadow">
         <img src="projects/logan/logan_1.jpg" class="img-responsive center-block img-border">
         <img src="projects/logan/logan_2.jpg" class="img-responsive center-block img-border">
         <img src="projects/logan/logan_3.jpg" class="img-responsive center-block img-border">
@@ -18,7 +17,6 @@
         <img src="projects/logan/logan_5.jpg" class="img-responsive center-block img-border">
         <img src="projects/logan/logan_6.jpg" class="img-responsive center-block img-border">
         <img src="projects/logan/logan_7.jpg" class="img-responsive center-block img-border">
-        <img src="projects/logan/logan_8.jpg" class="img-responsive center-block">
       </div>
     </div>
   </section>
